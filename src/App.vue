ee9cf68e6c839e310fd317b0a79a24acca92f5b4
Allen ZHU <linux0824@gmail.com>
2022-11-24 18:07:07 +0800

    把main.ts和App.vue折腾好了

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,20 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script lang="ts">
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+import { defineComponent } from 'vue'
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+export default defineComponent({
+  name: 'App'
+})
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+</script>
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<!--这是组合式api，上面是选项式api
+<script lang="ts" setup name='app'>
+</script>
+-->
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,30 @@
+// 导入vue框架里面的createApp函数
 import { createApp } from 'vue'
+
+// 导入本项目的App.vue组件
 import App from './App.vue'
 
+// 导入本项目的主css
 import './assets/main.css'
 
-createApp(App).mount('#app')
+// 导入element-plus框架里面所有的内容
+import ElementPlus from 'element-plus'
+
+// 导入element-plus本地化(中文)处理(本行导入出错)
+//import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+// 导入element-plus框架的css
+import 'element-plus/dist/index.css'
+
+// 创建vue app
+const app = createApp(App)
+
+// 使用element-plus框架/组件/库
+app.use(ElementPlus, { 
+    //zhCn,         // 中文设置
+    size: 'small', 
+    zIndex: 3000 
+})
+
+// 将app实例挂在到index.html中的#app上
+app.mount('#app')
